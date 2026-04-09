86589e0ac7dff558fbdeea0b07dfdf85de291f94
Marlon Santos <dejesusmarlon516@gmail.com>
2026-04-09 00:42:58 -0300

    backend -> frontend

---
+++ frontend/js/script.js
@@ -0,0 +1,35 @@
+const API = 'http://127.0.0.1:8000'
+
+const links = document.querySelectorAll('.header div nav ul li a');
+links.forEach(link => {
+        link.addEventListener('click', () => {links.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+    });
+}); 
+
+async function carregarDashboard() {
+    try{
+        const[pedidos, clientes, entregadores, encomendas] = await Promise.all([
+            fetch(`${API}/pedidos/?limit=1000`).then(r => r.json()),
+            fetch(`${API}/clientes/?limit=1000`).then(r => r.json()),
+            fetch(`${API}/entregadores/?limit=1000`).then(r => r.json()),
+            fetch(`${API}/encomendas/?limit=1000`).then(r => r.json()),
+        ])
+
+        const total = pedidos.length
+        const emTransito = pedidos.filter(p => p.status === 'Em trânsito').length
+        const entregues = pedidos.filter(p => p.status === 'Entregue').length
+        const atrasados = pedidos.filter( p => p.status === 'Cancelado').length
+
+
+        document.querySelectorAll('.card-value')[0].textContent = total
+        document.querySelectorAll('.card-value')[1].textContent = emTransito
+        document.querySelectorAll('.card-value')[2].textContent = entregues
+        document.querySelectorAll('.card-value')[3].textContent = atrasados
+
+    } catch(e){
+        console.error('Erro ao carregar dashboard:', e)
+    }
+}
+
+carregarDashboard()
--- frontend/js_/script.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll('.header div nav ul li a');
-
-links.forEach(link => {
-    link.addEventListener('click', () => {links.forEach(l => l.classList.remove('active'));
-link.classList.add('active');
-});
-}); 
